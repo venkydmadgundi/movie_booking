26d2be6d2f4a8647b5efa20d27ab133f01c9ebf4
venkydmadgundi <venky.v2@gmail.com>
2020-11-25 20:38:36 +0530

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'home#index'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
